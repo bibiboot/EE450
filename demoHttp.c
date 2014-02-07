fe2fbcbe59f054410e65aeec084bde6ee9e4d006
Danish Rehman <rehman.d.fazlur@gmail.com>
2014-02-06 20:07:59 -0800

    Adding socket

--- demoHttp.c
+++ demoHttp.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 char PORT[100] ;
 char HOST[100];
@@ -36,8 +39,39 @@ void print_init(){
     printf("WWWROOT: %s\n", WWWROOT);
 }
 
+void start(){
+
+    struct addrinfo hints, *res;
+    
+    memset(&hints, 0, 1000);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    getaddrinfo(HOST, PORT, &hints, &res);
+
+    int skid = socket(res->ai_family, res->ai_socktype, 0);
+
+    bind(skid, res->ai_addr, res->ai_addrlen);
+
+    listen(skid, 10);
+
+    while(1){
+
+        int new_sock = accept(skid, res->ai_addr, &(res->ai_addrlen));
+
+        send(new_sock, "Welcome new client\n", 1000, 0);
+
+        close(new_sock);
+
+    }
+
+    return 0;
+}
+
 int main(int argc, char *argv[]){
     init();
+
+    start();
 
     print_init();
 }
